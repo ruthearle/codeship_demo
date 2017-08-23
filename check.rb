bc5c588e252b6c1802192b6d99da54666f9db329
root <ruthearle@icloud.com>
2017-08-23 09:09:46 +0100

    trying to trigger docker

--- check.rb
+++ check.rb
@@ -1,12 +1,11 @@
-require "pg"
 require "redis"
+require "pg"
 
 def exit_if_not(expected, current)
   puts "Expected: #{expected}"
   puts "Current: #{current}"
   exit(1) if expected != current
 end
-
 
 puts "Redis"
 redis = Redis.new(host: "redis")
@@ -16,5 +15,4 @@ sleep 4
 postgres_username = "postgres"
 postgres_password = ""
 test = PG.connect("postgres", 5432, "", "", "postgres", postgres_username, postgres_password)
-
 puts test.exec("SELECT version();").first["version"]
